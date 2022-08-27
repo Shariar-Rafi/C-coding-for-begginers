31b67b59c11a812debf547a7ab39d7d859b7d2a8
Shariar Rafi <112250933+Shariar-Rafi@users.noreply.github.com>
2022-08-27 17:04:16 +0600

    Add files via upload

---
+++ Sum 2 integer number/sum 2 integer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Inter 2 integer number ");
+
+    scanf("%d %d", &a, &b);
+
+    sum=a+b;
+
+    printf("%d+%d=%d",a,b, sum);
+
+
+return 0;
+
+}
